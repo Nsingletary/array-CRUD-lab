65357d8d3afd16c2f02a50cf5a7d3158c9a83d93
Nsingletary <singletarybush@gmail.com>
2020-11-17 19:24:55 -0500

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,25 @@
-def array.unshift(element)
-    return array  
-end
-
-def create_an_array
-  
-end
-
-def add_element_to_end_of_array(array, element)
-  
-end
-
-def add_element_to_start_of_array(array, element)
-  
+array.unshift(element)
+return array
 end
 
 def remove_element_from_end_of_array(array)
-  
+return array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+return array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+element = array[index_number]
+return element
 end
 
 def retrieve_first_element_from_array(array)
-  
+return array[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
-end
 
 def retrieve_last_element_from_array(array)
-    return array[array.count - 1]
+return array[array.count - 1]
 end
